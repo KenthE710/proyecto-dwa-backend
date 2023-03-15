51bd315bfa4013a0f4b3095357dc42402b0cc22a
Kenth Encalada <kenencalada@gmail.com>
2023-03-15 04:41:13 -0500

    docker

--- Program.cs
+++ Program.cs
@@ -59,11 +59,12 @@ builder.Services.AddEndpointsApiExplorer();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+app.UseSwagger();
+app.UseSwaggerUI();
+
+// }
 
 app.UseHttpsRedirection();
 
